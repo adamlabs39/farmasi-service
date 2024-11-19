535180982f50744e832459e9c49baa65b3270ea3
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-11-19 10:01:30 +0700

    adjust konfigurasi harga

--- src/repositories/konfigurasi-harga-repository.js
+++ src/repositories/konfigurasi-harga-repository.js
@@ -1,21 +1,13 @@
-import BadRequestException from "../errors/bad-request-exception.js";
 import {KonfigurasiHargaModel} from "@adameds/model-sdk/farmasi";
 
 export default class KonfigurasiHargaRepository {
     static async get(faskes_uuid) {
-        const result = await KonfigurasiHargaModel.findOne({
+        return await KonfigurasiHargaModel.findOne({
             where: {
-                faskes_uuid : faskes_uuid,
+                faskes_uuid: faskes_uuid,
                 deleted_at: null,
             },
         });
-
-
-        if (!result){
-            throw new BadRequestException("Konfigurasi harga belum di set");
-        } else {
-            return result;
-        }
     }
 
     static async create(req) {
