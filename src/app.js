577f445394052aedf611951a42bad17e799caa1d
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-11-19 10:03:38 +0700

    adjust konfigurasi harga

--- src/app.js
+++ src/app.js
@@ -22,9 +22,9 @@ app.use(routes);
 app.use(errorMiddleware);
 app.listen(APPLICATION_PORT, APPLICATION_HOST, async () => {
     try {
-        // for (const model of MODELMERGE) {
-        //     await model.sync({ alter: false, force: true });
-        // }
+        for (const model of MODELMERGE) {
+            await model.sync({ alter: false, force: true });
+        }
         // //
         // //
         //
