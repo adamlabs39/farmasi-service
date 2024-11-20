28c3986255cbb1433cdc4154d52b8587e1ad0a8b
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-11-20 13:08:20 +0700

    model syncronize

--- src/models/model-synchronize.js
+++ src/models/model-synchronize.js
@@ -1,46 +1,61 @@
 import {
-    FpoPemberianModel, ItemMedisModel, LokasiStokModel,
+    AturanPakaiModel, BentukRacikanModel, BentukSediaanModel, CaraiPakaiModel, ConversionModel,
+    FpoPemberianModel,
+    HargaItemModel, IngredientItemMedisModel, IngredientModel,
+    ItemMedisJenisStokModel,
+    ItemMedisModel, ItemPenjualanObatModel,
+    JenisStokModel, KategoriObatModel, KonfigurasiHargaModel,
+    LokasiStokModel,
+    ManufactureModel,
     OrderAlkesItemModel,
     OrderAlkesModel,
-    PrescriptionItemModel, PrescriptionItemRacikanModel,
-    ReturItemModel, ReturModel, SatuanModel
+    PenjualanObatModel,
+    PrescriptionItemModel,
+    PrescriptionItemRacikanModel,
+    ReturItemModel,
+    ReturModel,
+    SatuanModel
 } from "@adameds/model-sdk/farmasi";
 import PrescriptionModel from "./prescription-model.js";
+import {LokasiModel} from "@adameds/model-sdk/datamaster";
+import {StockMedisModel} from "@adameds/model-sdk/inventory";
+import {PatientModel} from "@adameds/model-sdk/admisi";
+
+function JenisStokItemMedisModel() {
+    return undefined;
+}
 
 const MODELMERGE = [
-    // JenisStokModel,
-    // AturanPakaiModel,
-    // BentukRacikanModel,
-    // BentukSediaanModel,
-    // CaraiPakaiModel,
-    // ConversionModel,
-    // IngredientItemMedisModel,
-    // IngredientModel,
-    // ItemMedisModel,
-    // ItemPenjualanObatModel,
-    // JenisStokItemMedisModel,
-    // KategoriObatModel,
-    // KonfigurasiHargaModel,
-    // LokasiStokModel,
-    // ManufactureModel,
-    // OrderAlkesItemModel,
+    JenisStokModel,
+    AturanPakaiModel,
+    BentukRacikanModel,
+    BentukSediaanModel,
+    CaraiPakaiModel,
+    ConversionModel,
+    IngredientItemMedisModel,
+    IngredientModel,
+    ItemMedisModel,
+    ItemPenjualanObatModel,
+    JenisStokItemMedisModel,
+    KategoriObatModel,
+    KonfigurasiHargaModel,
+    LokasiStokModel,
+    ManufactureModel,
+    OrderAlkesItemModel,
     OrderAlkesModel,
-    // PatientModel,
-    // PenjualanObatModel,
-    // PrescriptionItemModel,
-    // PrescriptionItemRacikanModel,
+    PatientModel,
+    PenjualanObatModel,
+    PrescriptionItemModel,
+    PrescriptionItemRacikanModel,
     PrescriptionModel,
-    // ReturItemModel,
-    // ReturModel,
-    // SatuanModel,
-    // StockMedisModel,
-    // HargaItemModel,
-    // ItemMedisJenisStokModel,
-    // JenisStokModel,
-    // ItemMedisModel,
-    // FpoPemberianModel
-    // ReturItemModel,
-    // ReturModel
+    ReturItemModel,
+    ReturModel,
+    SatuanModel,
+    StockMedisModel,
+    HargaItemModel,
+    ItemMedisJenisStokModel,
+    FpoPemberianModel,
+    LokasiModel
 ];
 
 export default MODELMERGE;
