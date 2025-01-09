34bb86f5bc80c417661902ea03f4382979ec6a43
MahendraKhibrah <mahendrakrs448@gmail.com>
2025-01-09 17:48:51 +0700

    adjust laporan

--- src/controllers/report-controller.js
+++ src/controllers/report-controller.js
@@ -6,7 +6,7 @@ export default class ReportController {
         try {
             request.query.faskes_uuid = request.author.faskesUuid;
             const result = await ReportService.getPendapatan(request.query);
-            response.status(200).json(successResponse("data berhasil didapat",result))
+            response.status(200).json(successResponse("data berhasil didapat",result.data, result.pagination))
         } catch (error) {
             nextFunction(error);
         }
