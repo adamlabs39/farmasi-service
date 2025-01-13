6f54e79abfae733788b26f0f8a0ccb8c5c53e5a1
MahendraKhibrah <mahendrakrs448@gmail.com>
2025-01-13 13:16:21 +0700

    adjust laporan

--- src/validations/penjualan-obat-validation.js
+++ src/validations/penjualan-obat-validation.js
@@ -8,7 +8,7 @@ export default class PenjualanObatValidation {
         dokter_pemberi_resep: z.string().min(1, required),
         nama_pembeli: z.string().min(1, required),
         no_hp: z.string().min(1, required),
-        tanggal_pemberian  : z.number().min(1, required),
+        tanggal_pembelian  : z.number().min(1, required),
         no_transaksi: z.string().min(1, required),
     });
 
