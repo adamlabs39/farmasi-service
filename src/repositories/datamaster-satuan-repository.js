a09bc62046454d81dd1b6552d13d14cc77a3d615
Arif Rahman <arifrahman1997.aa@gmail.com>
2025-05-14 11:45:16 +0700

    fix: edit satuan

--- src/repositories/datamaster-satuan-repository.js
+++ src/repositories/datamaster-satuan-repository.js
@@ -1,88 +1,93 @@
 import Pagination from "../helpers/pagination.js";
-import {Op} from "sequelize";
-import {toEpochDate} from "../helpers/date-helper.js";
-import {SatuanModel} from "@adameds/model-sdk/farmasi";
+import { Op } from "sequelize";
+import { toEpochDate } from "../helpers/date-helper.js";
+import { SatuanModel } from "@adameds/model-sdk/farmasi";
 import BadRequestException from "../errors/bad-request-exception.js";
 import NotfoundException from "../errors/notfound-exception.js";
 
 export default class DataMasterSatuanRepository {
-    static async create(req) {
-        return await SatuanModel.create(req);
+  static async create(req) {
+    return await SatuanModel.create(req);
+  }
+
+  static async getAll(req) {
+    const option = {
+      where: {
+        faskes_uuid: req.faskes_uuid,
+        name: { [Op.iLike]: `%${req.name || ""}%` },
+        deleted_at: {
+          [Op.is]: null,
+        },
+      },
+    };
+
+    return Pagination.init(SatuanModel, req, option);
+  }
+
+  static async update(req) {
+    const [affectedRow] = await SatuanModel.update(
+      {
+        code: req.code,
+        name: req.name,
+        status: req.status,
+        satuan_dosis: req.satuan_dosis,
+        editable: req.editable,
+      },
+      {
+        where: {
+          uuid: req.uuid,
+        },
+      }
+    );
+
+    if (affectedRow === 0) {
+      throw new NotfoundException("Data gagal diedit");
     }
 
-    static async getAll(req) {
-        const option = {
-            where: {
-                faskes_uuid : req.faskes_uuid,
-                name : {[Op.iLike]: `%${req.name || ""}%`},
-                deleted_at: {
-                    [Op.is]: null,
-                },
-            },
-        };
+    return affectedRow;
+  }
 
-        return Pagination.init(SatuanModel, req, option);
+  static async delete(req) {
+    const [affectedRow] = await SatuanModel.update(
+      {
+        deleted_at: toEpochDate(new Date()),
+      },
+      {
+        where: {
+          uuid: req.uuid,
+        },
+      }
+    );
+
+    if (affectedRow === 0) {
+      throw new NotfoundException("Data gagal dihapus");
     }
 
-    static async update(req) {
-        const [affectedRow] =  await SatuanModel.update({
-            code: req.code,
-            name: req.name,
-            status: req.status,
-            satuan_dosis : req.satuan_dosis
-        }, {
-            where: {
-                uuid: req.uuid,
-            }
-        });
+    return affectedRow;
+  }
 
-        if (affectedRow === 0) {
-            throw new NotfoundException("Data gagal diedit");
-        }
+  static async getAllWithoutPagination(faskes_uuid) {
+    return await SatuanModel.findAll({
+      where: {
+        faskes_uuid: faskes_uuid,
+        satuan_dosis: true,
+      },
+    });
+  }
 
-        return affectedRow;
-    }
+  static async bulkCreate(data) {
+    return await SatuanModel.bulkCreate(data);
+  }
 
-    static async delete(req) {
-        const [affectedRow] = await SatuanModel.update({
-            deleted_at : toEpochDate(new Date())
-        },{
-            where: {
-                uuid: req.uuid,
-            }
-        });
-
-        if (affectedRow === 0) {
-            throw new NotfoundException("Data gagal dihapus");
-        }
-
-        return affectedRow;
-    }
-
-    static async getAllWithoutPagination(faskes_uuid) {
-        return await SatuanModel.findAll(
-            {
-                where: {
-                    faskes_uuid: faskes_uuid,
-                    satuan_dosis : true,
-                }
-            }
-        );
-    }
-
-    static async bulkCreate(data) {
-        return await SatuanModel.bulkCreate(data);
-    }
-
-    static async getUuidesByCodes(codes, faskesUuid) {
-        return await SatuanModel.findAll({
-            where: {
-                code: {
-                    [Op.in]: codes
-                },
-                faskes_uuid: faskesUuid,
-            },
-            attributes: ['uuid', 'code', 'name']
-        });
-    }
+  static async getUuidesByCodes(codes, faskesUuid) {
+    return await SatuanModel.findAll({
+      where: {
+        code: {
+          [Op.in]: codes,
+        },
+        faskes_uuid: faskesUuid,
+      },
+      attributes: ["uuid", "code", "name"],
+    });
+  }
 }
