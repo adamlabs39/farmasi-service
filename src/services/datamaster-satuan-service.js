94b3f09cf428d315b9246d7f9c954f5fa2ad6030
Arif Rahman <arifrahman1997.aa@gmail.com>
2025-05-14 11:24:48 +0700

    fix: import datamaster satuan

--- src/services/datamaster-satuan-service.js
+++ src/services/datamaster-satuan-service.js
@@ -4,30 +4,33 @@ import DataMasterSatuanRepository from "../repositories/datamaster-satuan-reposi
 import ExcelMapper from "../helpers/excel-mapper.js";
 
 export default class DatamasterSatuanService {
-    static async create(req) {
-        ZodValidator.validate(DatamasterValidation.CREATE_SATUAN, req);
-        req.code = req.code.toUpperCase();
-        return await DataMasterSatuanRepository.create(req);
-    }
+  static async create(req) {
+    ZodValidator.validate(DatamasterValidation.CREATE_SATUAN, req);
+    req.code = req.code.toUpperCase();
+    return await DataMasterSatuanRepository.create(req);
+  }
 
-    static async getAll(req) {
-        ZodValidator.validate(DatamasterValidation.GET_ALL_SATUAN, req);
-        return await DataMasterSatuanRepository.getAll(req);
-    }
+  static async getAll(req) {
+    ZodValidator.validate(DatamasterValidation.GET_ALL_SATUAN, req);
+    return await DataMasterSatuanRepository.getAll(req);
+  }
 
-    static update(req) {
-        ZodValidator.validate(DatamasterValidation.UPDATE_SATUAN, req);
-        return DataMasterSatuanRepository.update(req);
-    }
+  static update(req) {
+    ZodValidator.validate(DatamasterValidation.UPDATE_SATUAN, req);
+    return DataMasterSatuanRepository.update(req);
+  }
 
-    static delete(req) {
-        let validData = ZodValidator.validate(DatamasterValidation.DELETE_SATUAN, req);
-        return DataMasterSatuanRepository.delete(validData);
-    }
+  static delete(req) {
+    let validData = ZodValidator.validate(
+      DatamasterValidation.DELETE_SATUAN,
+      req
+    );
+    return DataMasterSatuanRepository.delete(validData);
+  }
 
-    static import(req){
-        const data = ExcelMapper.mapDatamasterSatuan(req.data, req.faskes_uuid);
+  static import(req) {
+    const data = ExcelMapper.mapDatamasterSatuan(req.data, req.faskes_uuid);
 
-        return DataMasterSatuanRepository.bulkCreate(data);
-    }
+    return DataMasterSatuanRepository.bulkCreate(data);
+  }
 }
